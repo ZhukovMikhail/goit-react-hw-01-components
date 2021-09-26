a903337a1e3c5c60c03c3872f713c51a04827d29
ZhukovMikhail <79285451+ZhukovMikhail@users.noreply.github.com>
2021-09-26 04:55:06 +0300

    done fix

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics stats={statisticalData} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </div>
